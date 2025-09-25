1968e5d38deadd23b87154f3ff750ac53dc9eba5
Prince Khurana <princekhuranadu@gmail.com>
2025-09-25 10:03:01 +0000

    there are no hover effect no visualzation look very stattics  (_for elem

--- src/components/homepage/ModuleGrid.tsx
+++ src/components/homepage/ModuleGrid.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import {
-  ArrowRight,
   Calculator,
   Cpu,
   Orbit,
@@ -12,7 +11,6 @@ import {
 import React, { useRef, useState, MouseEvent } from 'react';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
-import { cn } from '@/lib/utils';
 
 const gridItems = [
   {
@@ -61,27 +59,16 @@ const GridItem = ({
   index: number;
 }) => {
   const divRef = useRef<HTMLDivElement>(null);
-  const [isFocused, setIsFocused] = useState(false);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ x: -100, y: -100 });
   const [opacity, setOpacity] = useState(0);
 
   const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
-    if (!divRef.current || isFocused) return;
+    if (!divRef.current) return;
 
     const div = divRef.current;
     const rect = div.getBoundingClientRect();
 
     setPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
-  };
-
-  const handleFocus = () => {
-    setIsFocused(true);
-    setOpacity(1);
-  };
-
-  const handleBlur = () => {
-    setIsFocused(false);
-    setOpacity(0);
   };
 
   const handleMouseEnter = () => {
@@ -97,8 +84,6 @@ const GridItem = ({
       <motion.div
         ref={divRef}
         onMouseMove={handleMouseMove}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
         initial={{ opacity: 0, y: 20 }}
